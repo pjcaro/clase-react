64706b2bbaf6c6b0335f1db3f3817e6147d16325
Pablo Caro <pablo.caro@onetree.com>
2022-08-08 12:48:43 -0300

    ciclo de vida

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Title from './tittle';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      name: ""
+    }
+  }
+
+  // static getDerivedStateFromProps(props, state) {
+  //   return null
+  // }
+
+  // shouldComponentUpdate(newProps, newState) {
+  //   if (newState.name === "Pablo") {
+  //     return false
+  //   }
+  //   return true
+  // }
+
+  componentDidMount() {
+    // request al backend
+    setTimeout(() => {
+      this.setState({
+        name: "Pablo"
+      })
+    }, 2000)
+  }
+
+  componentDidUpdate() {
+    console.log('Ya se actualizo nuestro componente! ')
+  }
+
+  // componentWillUnmount() {
+  //   console.log('Will unmount')
+  // }
+
+  render() {
+    return (
+      <div className="App">
+        <Title text={this.state.name} />
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/tittle.jsx
@@ -0,0 +1,21 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+function Title({ text }) {
+    const [variable, setVariable] = useState('Hola')
+
+    useEffect(() => {
+        setTimeout(() => {
+            setVariable('Chau')
+        }, 3000)
+    }, [])
+
+    return (
+        <div>
+            <p>{variable}</p>
+            <code>{text != '' ? text : 'cargando'}</code>
+        </div>
+    );
+}
+
+export default Title
